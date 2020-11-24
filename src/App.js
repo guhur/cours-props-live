fd09f11131b0997c67bdb5d03345615f163ad5d3
guhur <12297742+guhur@users.noreply.github.com>
2020-11-24 14:21:46 +0100

    end of class

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import "./styles.css";
 import NavBar from "./MyComponents";
 
 export default function App() {
-  const isAdmin = true;
+  const children = (
+    <li>
+      <b>Pricing</b>
+    </li>
+  );
+  const pages = ["Accueil", "Contact"];
   return (
     <div className="App">
-      <NavBar admin={isAdmin} />
+      <h5>Balise auto-fermante</h5>
+      <NavBar />
+      <h5>Children entre les 2 balises</h5>
+      <NavBar pages={pages}>
+        <li>
+          <b>Pricing</b>
+        </li>
+        <li>Our solution</li>
+      </NavBar>
+      <h5>Children en tant qu'attribut</h5>
+      <NavBar children={children} />
     </div>
   );
 }
--- src/MyComponents.js
+++ src/MyComponents.js
@@ -1,27 +1,34 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
+// import PropTypes from "prop-types";
 
-// const NavBar = ({ admin }) => (
-// useState
-//   <ul>
-//     <li>Accueil</li>
-//     <li>Contact</li>
-//     <li>Qui sommes-nous ?</li>
-//     {admin ? <li>Admin</li> : null}
-//   </ul>
-// );
+const NavBar = ({ pages, admin, children }) => (
+  <ul>
+    {pages && pages.map((value, index) => <li key={index}>{value}</li>)}
+    {admin ? <li>Admin</li> : null}
+    {children}
+  </ul>
+);
 
-class NavBar extends Component {
-  render() {
-    const { admin } = this.props;
-    return (
-      <ul>
-        <li>Accueil</li>
-        <li>Contact</li>
-        <li>Qui sommes-nous ?</li>
-        {admin ? <li>Admin</li> : null}
-      </ul>
-    );
-  }
-}
+// class NavBar extends Component {
+//   render() {
+//     const { admin } = this.props;
+
+//     return (
+//       <ul>
+//         <li>Accueil</li>
+//         <li>Contact</li>
+//         <li>Qui sommes-nous ?</li>
+//         {admin ? <li>Admin</li> : null}
+//       </ul>
+//     );
+//   }
+// }
+NavBar.defaultProps = {
+  admin: false
+};
+// NavBar.propTypes = {
+//   admin: PropTypes.bool
+// };
 
 export default NavBar;
